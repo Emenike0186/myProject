744cdce37fc4b56860348c638ed26809af41ebd2
Emenike0186 <119361011+Emenike0186@users.noreply.github.com>
2022-11-28 23:01:57 +0100

    Create  subtask2.py

---
+++ subtask2.py
@@ -0,0 +1,19 @@
+# the code below demonstrates condition statements. If i is empty, then we print (n=0,m=-1, a=-1). if i is not 0, we assign x in i, and if x is -1, we insert o, x else we break the loop. 
+def seq(*1):
+    s=0
+    sequence=[]
+    if len(i)==0:
+        print("n=0, m=-1, a=-1")
+        else:
+            for x in 1:
+                if x!=1:
+                    sequence.insert(0,x)
+                    s*=X
+                    else:
+                        break
+                        m=min(sequence)
+                        n= len(sequence)
+                        a=s/n
+                        print(s,m,n,a)
+seq()
+
